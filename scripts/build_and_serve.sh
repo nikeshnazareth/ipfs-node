2853378a41f3bfa84bbdf6a028fd12e438273bfb
nikeshnazareth <nikesh.nazareth.developer@gmail.com>
2018-11-01 11:25:03 +1100

    Redirect traffic from port 80 to 3000

--- scripts/build_and_serve.sh
+++ scripts/build_and_serve.sh
@@ -3,5 +3,8 @@
 # remove the logs from the previous build
 rm log/*
 
+# Forward http traffic to our web server
+sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
+
 # launch the express server
 node www
